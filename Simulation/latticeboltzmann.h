4c11c7227cf9e3b1c95ef9e11f233364b46fc7e3
andrea <andsteinmora@gmail.com>
2020-04-07 15:22:03 -0500

    cambie un poco los codigos los ordene y agregue cometnarios

--- Simulation/latticeboltzmann.h
+++ Simulation/latticeboltzmann.h
@@ -11,10 +11,9 @@
 */
 
 
-const int proportion=3;
-const int Lx=20;
-const int Ly=4;
-const int Lz=100;
+const int Lr=20;
+const int Lt=4;
+const int Lz=50;
 
 const int Q=19;
 const double W0=1.0/3;
@@ -23,6 +22,8 @@ const double rmax= 200;
 const double rmin=2;
 const double zmin=0;
 const double zmax= 1200;
+const double thetamax=2*M_PI;
+const double thetamin=0;
 const double lambda= 4*zmax/9.0;
 
 const double C=0.5; // C<0.707 celdas/click
@@ -31,24 +32,31 @@ const double TresC2=3*C*C;
 const double AUX0=1-TresC2*(1-W0);
 const double Cs=0.5;
 const double Cs2=Cs*Cs;
+const double UCs2=1.0/Cs2;
 const double D=0.65;
-const double tau=0.5;
+const double tau=1;
 const double Utau=1.0/tau;
 const double UmUtau=1-Utau;
-const double deltar= (rmax-rmin)/Lx;
-const double deltatheta= 2*M_PI/Ly;
-const double deltatheta2=deltatheta*deltatheta;
-const double deltaz=zmax/Lz;
-const double deltas=deltaz*deltar*deltatheta;
+const double deltar= (rmax-rmin)/Lr;
+const double Udeltar=1.0/deltar;
+const double deltatheta= (thetamax-thetamin)/Lt;
+const double Udeltat=1.0/deltatheta;
+const double deltaz=(zmax-zmin)/Lz;
+const double Udeltaz=1.0/deltaz;
+const double deltas=deltar*deltatheta*deltaz;
 class LatticeBoltzmann
 {
 private:
   double w[Q];
   int V[3][Q]; // V[0][i]=V_ix,  V[1][i]=V_iy, V[2][i]=V_iz
-  double f[Lx][Ly][Lz][Q], fnew[Lx][Ly][Lz][Q]; // f[ix][iy][iz][iz][i]
+  double f[Lr][Lt][Lz][Q], fnew[Lr][Lt][Lz][Q]; // f[ix][iy][iz][iz][i]
 public:
   LatticeBoltzmann(void);
   double r(int i);	
+  double theta(int j);
+  double z(int k);
+  double Mg(int i, int j);
+  double Sig(int i , int j, int k);
   double rho(int ix,int iy,int iz,bool UseNew);
   double Jx(int ix,int iy,int iz,bool UseNew);
   double Jy(int ix,int iy,int iz,bool UseNew);
@@ -60,9 +68,7 @@ public:
   void ImponerCampos(int t);
   void Imprimase(const char * NombreArchivo);
   void Imprimir(int t, int ix, int iy, int iz, const char * NombreArchivo);
-  bool Columna(int x1, int x2, int x);
-  bool Rectangulo(int x1, int x2, int x, int y1, int y2, int y);
-  double GetRho(int ix, int iy, int iz, bool algo){double rho0 = rho(ix,iy,iz,algo);  return rho0;};
+  double GetRho(int ix, int iy, int iz, bool algo);
 };
 
 #endif // LATTICEBOLTZMANN_H
